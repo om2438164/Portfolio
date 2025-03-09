96dddc5c7e2b7818fdefe9b8787e88f748c4e9dc
ompatil <om2438164@gmail.com>
2025-03-09 17:10:30 +0530

    Added all files properly

---
+++ developer-portfolio/src/data/contactsData.js
@@ -0,0 +1,6 @@
+export const contactsData = {
+    email: 'patil4om@gmail.com',
+    phone: '+919309545032',
+    address: 'Nashik, Maharashtra 422201 ',
+    sheetAPI: ''
+}
---
+++ developer-portfolio/src/data/socialsData.js
@@ -0,0 +1,7 @@
+export const socialsData = {
+    github: 'https://github.com/om2438164',
+    linkedIn: 'https://www.linkedin.com/in/patil-om/',
+    twitter: 'https://twitter.com/',
+    leetcode: 'https://leetcode.com/u/Patil_Om/',
+    hackerrank: 'https://www.hackerrank.com/profile/om2438164',
+}
