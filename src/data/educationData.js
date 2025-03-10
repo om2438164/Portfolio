5e203b04cc5a013b24759d43bf249e348e130946
ompatil <om2438164@gmail.com>
2025-03-10 16:39:51 +0530

    education

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
     {
         id: 1,
-        institution: 'Marble Hills Middle School',
-        course: 'Higher Secondary Education',
-        startYear: '2007',
-        endYear: '2015'
+        institution: 'Dr. V. Vikhe Patil COE Ahmednagar',
+        course: 'Bachelor of Technology',
+        startYear: '2021',
+        endYear: 'Present'
     },
     {
         id: 2,
-        institution: 'Somerset School of Science',
-        course: 'Bachelor of Technology',
-        startYear: '2015',
-        endYear: '2019'
+        institution: 'Janata English Junior college Dindori',
+        course: 'Higher Secondary Education',
+        startYear: '2019',
+        endYear: '2021'
     },
     {
         id: 3,
-        institution: 'Hawking University, Cambridge',
-        course: 'Master of Technology',
-        startYear: '2019',
-        endYear: 'Present'
+        institution: 'K. K. Wagh Vidyabhavan B.nagar Nashik',
+        course: 'Secondary School Certificate',
+        startYear: '2014',
+        endYear: '2019'
     },
 ]
